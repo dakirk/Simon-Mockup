be487edb2f242c496b70fe9c0fd423b0f0679519
dakirk <dsak@bu.edu>
2016-10-18 23:14:09 -0400

    Uploaded Python code for game

---
+++ SimonSays.py
@@ -0,0 +1,36 @@
+import random
+import os
+import time
+
+gameRunning = True
+# Starting pattern length
+x = 4
+numList = []
+# Generates a random list consisting of numbers 1-4
+for i in range(0, x):
+    numList.append(random.randint(1, 4))
+
+
+while(gameRunning):
+    
+    submissionList = []
+
+    
+    print(numList)
+    time.sleep(5)
+    print ("\n" * 100)
+
+    # Gets player input, checks against list, and breaks if player makes a mistake
+    for i in range(0, x):
+        submissionList.append(eval(input()))
+        if(submissionList[i] != numList[i]):
+            break
+
+    # Checks if player has won and reacts accordingly
+    if(submissionList == numList):
+        print("You win!")
+        numList.append(random.randint(1, 4))
+        x += 1
+    else:
+        print("You lose!")
+        gameRunning = False
